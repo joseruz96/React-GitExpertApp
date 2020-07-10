78691711dc80d02c8ef5172e72f4a237e4dd8c4d
Jose <j.ruz.yanquepe@gmail.com>
2020-07-10 15:37:52 -0400

    Docs Added

---
+++ docs/precache-manifest.872c6354d6e93153366b8dcd1c4a793a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b4b9f381269ca03b862248035f4dac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2f16b3c4afe559d3af7",
+    "url": "/static/css/main.d33da563.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f16b3c4afe559d3af7",
+    "url": "/static/js/main.2e155964.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
